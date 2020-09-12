998f7aca6e5849ab10f04a008703652b7101b2d5
David <jlhuangch@gmail.com>
2020-09-13 00:43:37 +0800

    my scripts

---
+++ wp-change.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/ghibili
+feh --recursive  --bg-fill ~/Pictures/Linux-wallpapers-30.webp
+#feh --recursive --randomize --bg-fill ~/Pictures/wallpapers/view
